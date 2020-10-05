752f1881ccd1356f741781b0fe15ab37ac4835b6
mjbaek <bioinforac@gmail.com>
2020-10-05 16:22:55 +0900

    Update 201005

--- inst/scripts/test_script.R
+++ inst/scripts/test_script.R
@@ -8,10 +8,11 @@ output_file_name = '[Used_in_processes_except_for_annotation]'
 random_output_file_name = "[Result_with_random_analysis]"
 norandom_output_file_name = "[Result_without_random_analysis]"
 
-### 00. Make data files - Do it once at the first time of RIPAT.
-#makeData(organism = 'GRCh37')
+##### 00. Make data files - Do it once at the first time of running RIPAT.
+makeData(organism = 'GRCh37')
 
-### 01. Make R object
+##### 01. Make R object
+### Experimental data
 # Single file
 blat_obj = makeInputObj(inFile = paste0(input_dir, '/', input_file),
                         vectorPos = 'front',
@@ -25,34 +26,46 @@ blat_obj2 = makeInputObj2(inDir = input_dir,
                           vectorPos = 'front',
                           outPath = output_dir,
                           outFileName = paste0(output_file_name, '_multiple'))
+### Random data
+ran_obj = ranSetGenerator(organism = 'GRCh37',
+                          randomSize = 10000,
+                          outPath = output_dir,
+                          outFileName = output_file_name)
 
-### 02. Gene
-blat_gene_random = annoByGene(hits = blat_obj2, 
+##### 02. Integration frequency of each chromosome
+# Single input
+int_on_chr = integPatternByChr(hits = blat_obj, # it can be a list of exp_data
+                               ran_hits = ran_obj, # it can be a list of ran_data
+                               excelOut = TRUE,
+                               isExpList = FALSE, # TRUE if 'hits' is a list
+                               isRanList = FALSE, # TRUE if 'ran_hits' is a list
+                               outPath = output_dir,
+                               outFileName = output_file_name)
+
+##### 03. Gene
+blat_gene_random = annoByGene(hits = blat_obj2,
+                              ran_hits = ran_obj,
                               organism = 'GRCh37', 
                               interval = 5000,
                               range = c(-20000, 20000), 
-                              doRandom = TRUE, 
-                              randomSize = 5000, # default = 10000
                               includeUndecided = FALSE,
                               outPath = output_dir,
                               outFileName = random_output_file_name)
 
 blat_gene_norandom = annoByGene(hits = blat_obj,
-                                 organism = 'GRCh37',
-                                 interval = 5000,
-                                 range = c(-20000, 20000), 
-                                 doRandom = FALSE,
-                                 includeUndecided = FALSE,
-                                 outPath = output_dir,
-                                 outFileName = norandom_output_file_name)
+                                organism = 'GRCh37',
+                                interval = 5000,
+                                range = c(-20000, 20000),
+                                includeUndecided = FALSE,
+                                outPath = output_dir,
+                                outFileName = norandom_output_file_name)
 
-### 03. CpG
+##### 04. CpG
 blat_cpg_random = annoByCpG(hits = blat_obj,
+                            ran_hits = ran_obj,
                             organism = 'GRCh37',
                             interval = 5000,
                             range = c(-20000, 20000),
-                            doRandom = TRUE,
-                            randomSize = 5000, #default = 10000
                             includeUndecided = FALSE,
                             outPath = output_dir,
                             outFileName = random_output_file_name)
@@ -61,18 +74,16 @@ blat_cpg_norandom = annoByCpG(hits = blat_obj,
                               organism = 'GRCh37',
                               interval = 5000,
                               range = c(-20000, 20000),
-                              doRandom = FALSE,
                               includeUndecided = FALSE,
                               outPath = output_dir,
                               outFileName = norandom_output_file_name)
 
-### 04. Pathogenic variant
+##### 05. Pathogenic variant
 blat_var_random = annoByVar(hits = blat_obj,
+                            ran_hits = ran_obj,
                             organism = 'GRCh37',
                             interval = 5000,
                             range = c(-20000, 20000),
-                            doRandom = TRUE,
-                            randomSize = 5000, # default = 10000
                             includeUndecided = FALSE,
                             outPath = output_dir,
                             outFileName = random_output_file_name)
@@ -81,18 +92,16 @@ blat_var_norandom = annoByVar(hits = blat_obj,
                               organism = 'GRCh37',
                               interval = 5000,
                               range = c(-20000, 20000),
-                              doRandom = FALSE,
                               includeUndecided = FALSE,
                               outPath = output_dir,
                               outFileName = norandom_output_file_name)
 
-### 05. Repeat
+##### 06. Repeat
 blat_repeat_random = annoByRepeat(hits = blat_obj,
+                                  ran_hits = ran_obj,
                                   organism = 'GRCh37',
                                   interval = 5000,
                                   range = c(-20000, 20000),
-                                  doRandom = TRUE,
-                                  randomSize = 1000, # default = 10000
                                   includeUndecided = FALSE,
                                   outPath = output_dir,
                                   outFileName = random_output_file_name)
@@ -101,12 +110,11 @@ blat_repeat_norandom = annoByRepeat(hits = blat_obj,
                                     organism = 'GRCh37',
                                     interval = 5000,
                                     range = c(-20000, 20000),
-                                    doRandom = FALSE,
                                     includeUndecided = FALSE,
                                     outPath = output_dir,
                                     outFileName = norandom_output_file_name)
 
-### 06. Karyogram
+##### 07. Karyogram
 # Single file
 karyo_gene = drawingKaryo(hits = blat_obj,
                           organism = "GRCh37",
@@ -123,7 +131,7 @@ karyo_gene2 = drawingKaryo(hits = blat_obj,
                           outPath = output_dir,
                           outFileName = paste0(output_file_name, '_multiple'))
 
-### 07. Make result files
+##### 08. Make result files
 results = makeDocument(res = blat_gene_random,
                        resType = 'gene',
                        interval = 5000,
@@ -131,4 +139,5 @@ results = makeDocument(res = blat_gene_random,
                        includeUndecided = FALSE,
                        outPath = output_dir,
                        outFileName = paste0(output_file_name, '_multiple'))
+
 
